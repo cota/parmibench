0ac4c10b39ff5ec58a77d45b645deb509d463654
Emilio G. Cota <cota@braap.org>
2012-05-23 11:03:35 -0400

    Create scripts directory to store common scripts
    Signed-off-by: Emilio G. Cota <cota@braap.org>

--- dijkstra/Parallel/gen_defs.sh
+++ dijkstra/Parallel/gen_defs.sh
@@ -1,15 +1,1 @@
-#!/bin/bash
-
-OUTFILE=$1
-PROCESSORS=$2
-
-NAME=$(echo $1 | sed 's|\([^.]*\).*|\1|' | tr '[:lower:]' '[:upper:]')
-
-echo "#ifndef _${NAME}_H_" > $OUTFILE
-echo "#define _${NAME}_H_" >> $OUTFILE
-echo "" >> $OUTFILE
-
-echo "#define PROCESSORS $PROCESSORS" >> $OUTFILE
-
-echo "" >> $OUTFILE
-echo "#endif /* _${NAME}_H_ */" >> $OUTFILE
+../../scripts/gen_defs.sh
